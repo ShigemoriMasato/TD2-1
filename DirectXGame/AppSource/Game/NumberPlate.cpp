ed6be13333d9607d7307c0ba67d5418b9313d01c
ShigemoriMasato <k024g0027@g.neec.ac.jp>
2025-10-29 18:49:22 +0900

    時間対応

--- DirectXGame/AppSource/Game/NumberPlate.cpp
+++ DirectXGame/AppSource/Game/NumberPlate.cpp
@@ -26,15 +26,36 @@ void NumberPlate::Initialize(TextureManager* textureManager, int digit, bool isT
 		res_.back()->SetTextureHandle(textureManager->LoadTexture("Assets/Texture/number/colon.png"));
 	}
 
+	res_.emplace_back(std::make_unique<DrawResource>());
+	res_.back()->Initialize(ShapeType::Plane);
+	res_.back()->camera_ = camera_.get();
+	res_.back()->SetTextureHandle(textureManager->LoadTexture("Assets/Texture/number/minas.png"));
+
 	transform_.scale = { 0.7f, 0.7f, 1.0f };
 	transform_.position = { -4.1f, -3.7f, 20.0f };
 }
 
 void NumberPlate::Update(int num) {
 	std::vector<int> digits;
-	for(int i = 0; i < digit_; ++i) {
-		digits.push_back(num % 10);
-		num /= 10;
+
+	if (isTimer_) {
+
+		int sec = num % 60;
+		if (digits.size() > 0)digits.push_back(sec % 10);
+		if (digits.size() > 1)digits.push_back(sec / 10);
+		int minutes = num / 60;
+		for (int i = 2; i < digit_; ++i) {
+			digits.push_back(minutes % 10);
+			minutes /= 10;
+		}
+
+	} else {
+
+		for (int i = 0; i < digit_; ++i) {
+			digits.push_back(num % 10);
+			num /= 10;
+		}
+
 	}
 
 	for (int i = 0; i < digit_; ++i) {
@@ -52,10 +73,25 @@ void NumberPlate::Update(int num) {
 	}
 
 	if (isTimer_) {
+		res_[(int)res_.size() - 1]->position_ = transform_.position;
+		res_[(int)res_.size() - 1]->position_.x -= transform_.scale.x + transform_.scale.x * 0.5f;
+		res_[(int)res_.size() - 1]->rotate_ = transform_.rotation;
+		res_[(int)res_.size() - 1]->scale_ = transform_.scale;
+	}
+
+	//マイナス表示
+	if (num < 0) {
 		res_.back()->position_ = transform_.position;
-		res_.back()->position_.x -= transform_.scale.x + transform_.scale.x * 0.5f;
+		if (isTimer_) {
+			res_.back()->position_.x -= (digit_ * transform_.scale.x) + 0.8f * transform_.scale.x;
+		} else {
+			res_.back()->position_.x -= digit_ * transform_.scale.x;
+		}
 		res_.back()->rotate_ = transform_.rotation;
 		res_.back()->scale_ = transform_.scale;
+		res_.back()->color_ = 0xffffffff;
+	} else {
+		res_.back()->color_ = 0x00000000;
 	}
 
 	ImGui::Begin("NumberPlate");
